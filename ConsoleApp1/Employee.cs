8adbbe3d374a874389be7fa1af537ab18b6f3232
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2021-07-12 20:38:48 +0200

    Initial commit

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Employee
+        {
+            public String LastName { get; set; }
+            public String PostalCode { get; set; }
+            public String Address { get; set; }
+
+        }
+    }
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.ScaffoldModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            using var ctx = new NorthwindContext();
+            ctx.Add(new Employee() {  LastName = "Kowalski", Address = "Laczna 15", PostalCode="2222"});
+            Console.WriteLine(ctx.ChangeTracker.DebugView);
+            //ctx.SaveChanges();
+        }
+    }
+}
+//The entity type 'Employee' was not found. Ensure that the entity type has been added to the model.”
